14e11a530fbbf45475f2abf835d60dd0bd7b66de
aykut <96610819+mrjjmc@users.noreply.github.com>
2024-02-18 02:32:26 +0100

    Update build-changelog.js

--- build-changelog.js
+++ build-changelog.js
@@ -1,97 +1,26 @@
-const fs = require('fs')
+const fs = require('fs').promises;
 
-let value = fs.readFileSync('./change-log.json')
-value = JSON.parse(value)
-const inDone = value.data.search.edges[0].node.project.columns.edges[0].node.cards.edges
-let data = ''
-const prCount = inDone.filter((card) => {
-  return card.node.content.url
-}).length
-console.log(prCount, 'PRs found!\n')
-data = prCount + ' PRs found!\n\n'
-for (let card of inDone) {
-    if (card.node.content.url && card.node.content.merged) {
-      data += ` - [${card.node.content.title}](${card.node.content.url}) (@${card.node.content.author.login})\n`
-      data += `       participants: (${card.node.content.participants.edges.map((p) => { return ` @${p.node.login}` })})`
-      data += '\n\n'
-    }
-}
-console.log('change-log.txt updated')
-fs.writeFileSync('./change-log.txt', data)
+(async () => {
+  try {
+    const jsonContent = await fs.readFile('./change-log.json');
+    const data = JSON.parse(jsonContent);
+    
+    const cards = data.data.search.edges[0].node.project.columns.edges[0].node.cards.edges;
 
-/*
- - Go to https://docs.github.com/en/graphql/overview/explorer
- - Set the correct project id
- - Run the below mentioned query (be careful, this only returns the first 100 elements)
- - Save the JSON content as change-log.json
- - Run this script i.e. build-changelog.js
- - Use the result from the file change-log.txt to write changelog on Github release
-*/
-/*
+    const prCards = cards.filter(card => card.node.content.url && card.node.content.merged);
+    const prCount = prCards.length;
+    
+    let changelog = `${prCount} PRs found!\n\n`;
+    
+    prCards.forEach(card => {
+      const { title, url, author, participants } = card.node.content;
+      changelog += `- [${title}](${url}) (@${author.login})\n`;
+      changelog += `  participants: (${participants.edges.map(p => ` @${p.node.login}`)})\n\n`;
+    });
 
-{
-  search(type: REPOSITORY, query: "remix-project", first: 1) {
-    edges {
-      node {
-        __typename
-        ... on Repository {
-          owner {
-            id
-          }
-          name
-          project(number: 31) {
-            number
-            name
-            columns(last: 1) {
-              edges {
-                node {
-                  name
-                  cards(first: 100) {
-                    edges {
-                      cursor
-                      node {
-                        id
-                        note                        
-                        state
-                        content {
-                          ... on PullRequest {
-                            participants (last: 100) {
-                              edges {
-                                node {
-                                  id
-                                  login
-                                }
-                              }
-                            }
-                            author {
-                              login
-                            }
-                            assignees (last: 100) {
-                              edges {
-                                node {
-                                  id
-                                  login
-                                }
-                              }
-                            }
-                            url
-                            merged
-                            id
-                            number
-                            title
-                          }
-                        }
-                      }
-                    }
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-    }
+    await fs.writeFile('./change-log.txt', changelog);
+    console.log('change-log.txt updated');
+  } catch (error) {
+    console.error('Error occurred:', error);
   }
-}
-
-*/
+})();
